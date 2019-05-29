083c16bd7fac60ebc15e614c2d33476911910d84
vijayhk94 <vijayhk94@gmail.com>
2019-05-29 10:28:58 -0700

    User Profile and Picture, login and logout

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
